e535bbcd1bf33f58052ed57f9460afe120232229
DezeDev <145411663+DezeDev@users.noreply.github.com>
2023-09-27 11:39:36 -0300

    Projeto - Apresentação
    Projeto para apresentação

---
+++ VFfitness/script.js
@@ -0,0 +1,7 @@
+const hambu = document.querySelector(".hambu");
+const navMenu = document.querySelector(".nav-menu");
+
+hambu.addEventListener("click", () => {
+    hambu.classList.toggle("active");
+    navMenu.classList.toggle("active");}
+    )
